71050daf2baa32f96b8d23c998e298569a6e8b80
Kabina Thapa <thapakabina28@gmail.com>
2023-11-14 15:46:26 -0500

    feat:display default ip address of users and provide search functionality based on users input

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-
-    // Your existing code here
+const userInput = document.getElementById('input').value;
+fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_mxkNkLWs7MBO2zg1rNk97bnEjqSmQ&ipAddress=${encodeURIComponent(userInput)}`)
+        .then((response) => response.json())
+        .then(data => {
+          console.log(data);
+          // Update the displayed information on your web page
+          document.getElementById('ip').textContent = data.ip;
+          document.getElementById('location').textContent = data.location.region;
+          document.getElementById('timezone').textContent = data.location.timezone;
+          document.getElementById('isp').textContent = data.isp;
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
     document.querySelector('.searchbar').addEventListener('submit', function (event) {
       event.preventDefault();
       const userInput = document.getElementById('input').value;
-     
-      fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_mxkNkLWs7MBO2zg1rNk97bnEjqSmQ&ipAddress=${encodeURIComponent(userInput)}`)
+      const ipAddress=userInput===null?'':encodeURIComponent(userInput)
+      fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_mxkNkLWs7MBO2zg1rNk97bnEjqSmQ&ipAddress=${ipAddress}`)
         .then((response) => response.json())
         .then(data => {
           console.log(data);
