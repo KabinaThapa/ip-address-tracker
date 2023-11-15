375dcca22e4a6e254f8a5d0ffe991b3667300fbe
Kabina Thapa <thapakabina28@gmail.com>
2023-11-15 13:40:57 -0500

    feat:implement leaflet to create a map

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-const userInput = document.getElementById('input').value;
-fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_mxkNkLWs7MBO2zg1rNk97bnEjqSmQ&ipAddress=${encodeURIComponent(userInput)}`)
+
+        document.addEventListener('DOMContentLoaded', function(){
+            const userInput = document.getElementById('input').value;
+fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_mxkNkLWs7MBO2zg1rNk97bnEjqSmQ&ipAddress=${encodeURIComponent(userInput)}`)
         .then((response) => response.json())
         .then(data => {
           console.log(data);
           // Update the displayed information on your web page
           document.getElementById('ip').textContent = data.ip;
-          document.getElementById('location').textContent = data.location.region;
+          document.getElementById('location').textContent =  `${data.location.city},${data.location.region}, ${data.location.postalCode}`
           document.getElementById('timezone').textContent = data.location.timezone;
           document.getElementById('isp').textContent = data.isp;
+           updateMap(data.location.lat, data.location.lng)
         })
         .catch(error => {
           console.error('Error:', error);
         });
+            const map = L.map('map').setView([0, 0], 13); // Default view is set to (0, 0)
+
+  // Add a tile layer from OpenStreetMap
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+  // Function to update the map with a marker at the specified coordinates
+  function updateMap(lat, lng) {
+    map.setView([lat, lng], 13); // Set the view to the specified coordinates
+    L.marker([lat, lng]).addTo(map);
+  }
+
+    
     document.querySelector('.searchbar').addEventListener('submit', function (event) {
       event.preventDefault();
-      const userInput = document.getElementById('input').value;
-      const ipAddress=userInput===null?'':encodeURIComponent(userInput)
-      fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_mxkNkLWs7MBO2zg1rNk97bnEjqSmQ&ipAddress=${ipAddress}`)
+     
+      
+      fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_mxkNkLWs7MBO2zg1rNk97bnEjqSmQ&ipAddress=${userInput}`)
         .then((response) => response.json())
         .then(data => {
           console.log(data);
@@ -25,10 +43,12 @@ fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_mxkNkLWs7MBO2zg1rNk97bnEjq
           document.getElementById('location').textContent = data.location.region;
           document.getElementById('timezone').textContent = data.location.timezone;
           document.getElementById('isp').textContent = data.isp;
+          updateMap(data.location.lat, data.location.lng)
+         
         })
         .catch(error => {
           console.error('Error:', error);
         });
     });
-
+})
   
